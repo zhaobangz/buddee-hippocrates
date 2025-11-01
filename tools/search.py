e371787a447f28259b6f89aaf4af23b507cc9990
zhuzhaob <zhuzhaob@stanford.edu>
2025-11-01 15:38:28 -0700

    summarize results func added

--- tools/search.py
+++ tools/search.py
@@ -2,6 +2,12 @@ import requests
 from googlesearch import search as google_search
 from core.config import Config
 
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+
 class WebSearch:
     def __init__(self, num_results=3, search_engine='google', summarization=True, cache=True):
         self.num_results = num_results
@@ -9,6 +15,22 @@ class WebSearch:
         self.summarization = summarization
         self.cache = cache
         self.search_results = {}
+
+    def summarize_results(self, results):
+        # Combine all the results into a single text
+        text = '\n\n'.join(results)
+
+        # Summarize the text using LSA
+        parser = PlaintextParser.from_string(text, Tokenizer('english'))
+        stemmer = Stemmer("english")
+        summarizer = LsaSummarizer(stemmer)
+        summarizer.stop_words = get_stop_words('english')
+        summary = summarizer(parser.document, 2)  # summarize to 2 sentences
+
+        # Join the summary sentences into a single string
+        summary_text = ' '.join([sent.sentences[0] for sent in summary])
+
+        return summary_text
 
     def search(self, query):
         if self.cache and query in self.search_results:
@@ -26,7 +48,7 @@ class WebSearch:
             if results:
                 if self.summarization:
                     # Use a summarization algorithm to condense the results into a shorter, more digestible form
-                    summary = summarize_results(results)
+                    summary = summarize_results(self, results)
                     self.search_results[query] = summary
                     return summary
                 else:
