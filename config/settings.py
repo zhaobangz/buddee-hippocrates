c34ac690ad87283d2c1a8c54fa8a1299fd951a5f
zhuzhaob <zhuzhaob@stanford.edu>
2025-11-10 15:39:36 -0800

    use of key

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ Use_VOICE  = False #toggle speech mode
 MEMORY_ENALED = True # toggle memory module
 
 # when setting an api key choose deepseek or openai 
-api_key = os.getenv("MY_API_KEY")
+api_key = os.getenv("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")
 url = f"https://api.example.com/data?param=value&apiKey={api_key}"
 response = requests.get(url)
 
